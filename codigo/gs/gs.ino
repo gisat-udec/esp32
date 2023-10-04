440277a64a21092363ef4798f4b61b3e4bf27f50
qgerman2 <qgerman2@gmail.com>
2023-10-03 23:46:19 -0300

    gs

--- codigo/gs/gs.ino
+++ codigo/gs/gs.ino
@@ -4,50 +4,17 @@
 #include <Wire.h>
 
 #include <vector>
-#include <cstring>
-#include <atomic>
 
-#include "src/sensor/sensor.hpp"
 #include "src/wifi.hpp"
 
-#define LED_PIN 33
-std::atomic<uint32_t> packets_sent;
-std::vector<Sensor *> sensors;
 void setup() {
-	packets_sent = 0;
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
 	Serial.begin(115200);
 	Wire.begin(14, 15);
 	Wire.setClock(400000);
-	sensors.push_back(dynamic_cast<Sensor *>(new BNO080_c()));
-	sensors.push_back(dynamic_cast<Sensor *>(new BME680_c()));
-	sensors.push_back(dynamic_cast<Sensor *>(new Camera_c()));
-	sensors.push_back(dynamic_cast<Sensor *>(new GPS_c()));
 	wifi();
-	pinMode(LED_PIN, OUTPUT);
-	TimerHandle_t logtimer = xTimerCreate(
-		"log", pdMS_TO_TICKS(1000), pdTRUE, NULL,
-		[](TimerHandle_t xTimer) {
-			Serial.print("\npkt tx: ");
-			Serial.print(packets_sent);
-			packets_sent = 0;
-			digitalWrite(LED_PIN, LOW);
-			vTaskDelay(pdMS_TO_TICKS(100));
-			digitalWrite(LED_PIN, HIGH);
-		});
-	xTimerStart(logtimer, 0);
 }
 
-std::vector<Packet> tosend;
 IRAM_ATTR void loop() {
-	for (Sensor *sensor : sensors) {
-		if (sensor->available()) {
-			tosend.push_back(sensor->get());
-			packets_sent++;
-		}
-	}
-	if (!tosend.empty()) {
-		send(tosend);
-		tosend.clear();
-	}
+	vTaskDelay(1000);
 }
---
+++ codigo/gs/src/wifi.cpp
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_wifi.h>
+#include <esp_private/wifi.h>
+
+#include <vector>
+#include <cstring>
+
+#include "packet.hpp"
+#include "wifi.hpp"
+
+#define MTU 1500
+
+TaskHandle_t handle;
+uint8_t tx_buf[MTU];
+
+void wifi() {
+	ESP_ERROR_CHECK(esp_wifi_internal_set_log_level(WIFI_LOG_NONE));
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_start());
+	ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+	ESP_ERROR_CHECK(esp_wifi_config_80211_tx_rate(WIFI_IF_STA, WIFI_PHY_RATE_11M_S));
+	ESP_ERROR_CHECK(esp_wifi_set_channel(11, WIFI_SECOND_CHAN_NONE));
+	wifi_promiscuous_filter_t filter =
+	{
+		.filter_mask = WIFI_PROMIS_FILTER_MASK_DATA
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous_filter(&filter));
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous_ctrl_filter(&filter));
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous(true));
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous_rx_cb(rx_callback));
+	ESP_ERROR_CHECK(esp_wifi_set_tx_done_cb(tx_callback));
+	ESP_ERROR_CHECK(esp_wifi_set_max_tx_power(80));
+	std::copy_n(dot11_header, sizeof(dot11_header), tx_buf);
+	handle = xTaskGetCurrentTaskHandle();
+}
+
+IRAM_ATTR void rx_callback(void *buf, wifi_promiscuous_pkt_type_t type) {
+	auto *packet = reinterpret_cast<wifi_promiscuous_pkt_t *>(buf);
+
+	// Filtrar paquetes por MAC
+	if (std::memcmp(&packet->payload[4], address1, 6) != 0) { return; }
+	if (std::memcmp(&packet->payload[10], address2, 6) != 0) { return; }
+	if (std::memcmp(&packet->payload[16], address3, 6) != 0) { return; }
+
+	// Enviar por serial
+	Serial.print("\ngot packet\n");
+	for (size_t i = 0; i < packet->rx_ctrl.sig_len; i++) {
+		Serial.print(packet->payload[i], HEX);
+	}
+
+}
+
+IRAM_ATTR void tx_callback(uint8_t ifidx, uint8_t *data, uint16_t *data_len, bool txStatus) {
+	xTaskNotifyGive(handle);
+}
